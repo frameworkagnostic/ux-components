07dd4d8feebc4bbaff199fc819151d2adf70b48f
carlosmarte <opensource@carlosmarte.me>
2017-09-15 07:14:10 -0400

    save

---
+++ react/README.md
@@ -0,0 +1,4 @@
+https://formidable.com/open-source/radium/docs/api#style-component
+
+https://github.com/postcss/postcss
+https://www.postcss.parts/
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,3 +1,4 @@
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
     entry: './src/index.js',
@@ -12,17 +13,39 @@ module.exports = {
               use : 'babel-loader'
           },
           {
-            test: /\.scss$/,
+            test: /\.css$/,
             use: [
+              // require.resolve('style-loader'),
+              // {
+              //   loader: require.resolve('css-loader'),
+              //   options: {
+              //     importLoaders: 1,
+              //   },
+              // },
+              require.resolve('./components/css-json/src'),
+
               {
-                  loader: "style-loader"
-              }, {
-                  loader: "css-loader"
-              }, {
-                  loader: "sass-loader"
-              }
-            ]
-          }
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              },
+            ],
+          },
         ]
     },
     plugins: []
